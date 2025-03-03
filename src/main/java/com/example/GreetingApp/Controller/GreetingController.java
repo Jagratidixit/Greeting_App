fa7a5e8bd7308c3355553eb4a4f5366be17b39c9
JagratiDixit <jagratidixit2010@gmail.com>
2025-03-04 00:00:46 +0530

    GreetingController return JSON for different HTTP Methods. Test using curl

---
+++ src/main/java/com/example/GreetingApp/Controller/GreetingController.java
@@ -0,0 +1,30 @@
+package com.example.GreetingApp.Controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public String getGreeting() {
+        return "{\"message\": \"Hello from GET\"}";
+    }
+
+    @PostMapping
+    public String postGreeting() {
+        return "{\"message\": \"Hello from POST\"}";
+    }
+
+    @PutMapping
+    public String putGreeting() {
+        return "{\"message\": \"Hello from PUT\"}";
+    }
+
+    @DeleteMapping
+    public String deleteGreeting() {
+        return "{\"message\": \"Hello from DELETE\"}";
+    }
+}
+
