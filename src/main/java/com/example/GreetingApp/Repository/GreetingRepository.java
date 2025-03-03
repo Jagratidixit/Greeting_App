132828e637fa158d706ac78eef0752207b55e139
JagratiDixit <jagratidixit2010@gmail.com>
2025-03-04 00:51:10 +0530

    Ability for the Greeting App to save the Greeting Message in the Repository

---
+++ src/main/java/com/example/GreetingApp/Repository/GreetingRepository.java
@@ -0,0 +1,8 @@
+package com.example.GreetingApp.Repository;
+
+
+import com.example.GreetingApp.Model.Greeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GreetingRepository extends JpaRepository<Greeting, Long> {
+}
--- src/main/java/com/example/GreetingApp/Service/GreetingService.java
+++ src/main/java/com/example/GreetingApp/Service/GreetingService.java
@@ -1,19 +1,21 @@
 package com.example.GreetingApp.Service;
 
+
+import com.example.GreetingApp.Model.Greeting;
+import com.example.GreetingApp.Repository.GreetingRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
-    public String getGreeting(String firstName, String lastName) {
-        if (firstName != null && lastName != null) {
-            return "{\"message\": \"Hello " + firstName + " " + lastName + "\"}";
-        } else if (firstName != null) {
-            return "{\"message\": \"Hello " + firstName + "\"}";
-        } else if (lastName != null) {
-            return "{\"message\": \"Hello " + lastName + "\"}";
-        } else {
-            return "{\"message\": \"Hello World\"}";
-        }
+
+    private final GreetingRepository repository;
+
+    public GreetingService(GreetingRepository repository) {
+        this.repository = repository;
+    }
+
+    public Greeting saveGreeting(String message) {
+        Greeting greeting = new Greeting(message);
+        return repository.save(greeting);
     }
 }
-
